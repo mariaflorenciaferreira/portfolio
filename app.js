7ccd3e2f6beddc3445766a85be23d926a97f3e7f
mariaflorenciaferreira <florferreirag@gmail.com>
2022-05-18 22:48:36 -0300

    light mode color correction

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ PageTransitions();
 
 const checkbox=document.getElementById('checkbox');
 checkbox.addEventListener('change',()=>{
-    document.body.classList.toggle('dark')
+    document.body.classList.toggle('light')
   
 })
+
